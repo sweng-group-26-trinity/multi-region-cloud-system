f7c764bd1ec49bde2213774b4a61ec59bb657dd2
dschwer <schwerd@tcd.ie>
2026-03-10 16:05:37 +0000

    added routing

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,9 @@
+// src/components/ProtectedRoute.tsx
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export default function ProtectedRoute() {
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+}
